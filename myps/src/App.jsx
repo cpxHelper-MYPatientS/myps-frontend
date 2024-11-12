630f9b8a276da2cc0cd86909912859762f0304a4
yoonjin-C <cyy000123@gmail.com>
2024-11-12 11:33:48 +0900

    🎉 프로젝트 기본 세팅

---
+++ myps/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className='m-auto text-red-600'>
+      안녕하세요. 의사 국가고시 실기 시험 CPX를 위한 모의 진료 플랫폼, 마이피스입니다. 
+      </div>
+    </>
+  )
+}
+
+export default App
