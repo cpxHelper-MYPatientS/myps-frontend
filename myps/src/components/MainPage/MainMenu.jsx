32c93d728e9019873fbe2dfe898475d805d76f27
yoonjin-C <cyy000123@gmail.com>
2024-11-15 21:46:34 +0900

    ✨ Feat: 라우팅 추가

--- myps/src/components/MainPage/MainMenu.jsx
+++ myps/src/components/MainPage/MainMenu.jsx
@@ -1,11 +1,12 @@
 import grad from "../../assets/mainPage/grad.svg";
 import circle from "../../assets/mainPage/circle.svg";
 import arrowIcon from "../../assets/mainPage/arrow_outward.svg";
-const MainMenu = ({ title, iconSrc, isSub = false }) => {
+const MainMenu = ({ title, iconSrc, isSub = false, onClick }) => {
   return (
     <div
       className="flex flex-col w-[24.1875rem] h-[21.75rem] px-10 pt-10 pb-9 justify-between bg-white shadow-custom2 rounded-[0.625rem] cursor-pointer"
       style={{ backgroundImage: `url(${grad})` }}
+      onClick={onClick}
     >
       <div
         className="flex justify-center items-center w-[60px] h-[60px]"
--- myps/src/components/common/NavBar.jsx
+++ myps/src/components/common/NavBar.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Logo from "../../assets/logo.svg";
 import AlarmIcon from "../../assets/common/alarm.svg";
 import CouponIcon from "../../assets/common/coupon.svg";
 import UserIcon from "../../assets/common/user.svg";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate("/");
+  };
   const categories = [
     { img: AlarmIcon, text: "알림" },
     { img: CouponIcon, text: "이용권 구매" },
@@ -11,7 +16,7 @@ const NavBar = () => {
   ];
   return (
     <div className="flex justify-between px-[22.5rem] py-4 border-b border-b-violet-10 shadow-custom1 bg-white w-full h-20">
-      <img src={Logo} className="cursor-pointer" />
+      <img src={Logo} className="cursor-pointer" onClick={handleLogoClick} />
       <div className="flex gap-5">
         {categories.map((category, index) => (
           <div key={index} className="flex items-center gap-1 cursor-pointer">
