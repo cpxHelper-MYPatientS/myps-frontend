3cbd78ea2a1315ada3022e2add104ffc5646aaf1
yoonjin-C <cyy000123@gmail.com>
2024-11-15 22:19:56 +0900

    ✏️ Fix: 네비바 버튼 추가

--- myps/src/pages/CasePage/CaseDetailPage.jsx
+++ myps/src/pages/CasePage/CaseDetailPage.jsx
@@ -4,7 +4,7 @@ import CaseDetailView from "../../components/CasePage/CaseDetailView";
 const CaseDetailPage = () => {
   return (
     <div className="flex flex-col pb-10">
-      <NavBar />
+      <NavBar isMain={false} />
       <div className="flex flex-col gap-8 mt-[5.1875rem] mx-[22.5rem]">
         <CaseTitle />
         <CaseDetailView />
