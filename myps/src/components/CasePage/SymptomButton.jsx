6c6834cf5762de24de01743d83ca74b356671121
yoonjin-C <cyy000123@gmail.com>
2024-11-19 17:35:37 +0900

    💄 UI: 체크리스트 달성률 모달창 퍼블리싱

--- myps/src/components/CasePage/SymptomButton.jsx
+++ myps/src/components/CasePage/SymptomButton.jsx
@@ -1,6 +1,6 @@
 const CheckButton = () => {
   return (
-    <button className="ml-[3.875rem] px-2 py-3 bg-cgray-50 font-medium text-cgray-700 w-[99px] rounded-[5px]">
+    <button className="ml-[3.875rem] px-2 py-3 bg-cgray-50 font-medium text-cgray-700 w-[6.1875rem] rounded-[0.3125rem]">
       확인하기
     </button>
   );
@@ -8,23 +8,29 @@ const CheckButton = () => {
 
 const MakeButton = () => {
   return (
-    <button className="ml-[105px] px-2 py-3 bg-cgray-50 font-medium text-cgray-700 w-[99px] rounded-[5px]">
+    <button className="ml-[6.5625rem] px-2 py-3 bg-cgray-50 font-medium text-cgray-700 w-[6.1875rem] rounded-[0.3125rem]">
       제작하기
     </button>
   );
 };
 
-const CompleteRate = () => {
+const CompleteRate = ({ onHover }) => {
   return (
-    <div className="ml-[113px] px-2 py-3 bg-violet-25 font-bold text-violet-600 text-center w-[82px] rounded-[5px] cursor-pointer">
-      30%
-    </div>
+    <>
+      <div
+        className="ml-[7.0625rem] px-2 py-3 bg-violet-25 font-bold text-violet-600 text-center w-[5.125rem] rounded-[0.3125rem] cursor-pointer"
+        onMouseEnter={() => onHover(true)}
+        onMouseLeave={() => onHover(false)}
+      >
+        30%
+      </div>
+    </>
   );
 };
 
 const CompletePatientNum = () => {
   return (
-    <div className="ml-[112px] px-2 py-3 bg-blue-10 font-bold text-blue-600 text-center w-[99px] rounded-[5px]">
+    <div className="ml-[7rem] px-2 py-3 bg-blue-10 font-bold text-blue-600 text-center w-[6.1875rem] rounded-[0.3125rem]">
       19/20
     </div>
   );
