53cef1a7f7e14feedf93dc21e6bd81919679c35c
yoonjin-C <cyy000123@gmail.com>
2024-11-19 18:11:27 +0900

    💄 UI: 환자 케이스 버튼 추가

--- myps/src/components/common/PatientCase.jsx
+++ myps/src/components/common/PatientCase.jsx
@@ -1,9 +1,43 @@
+import deleteIcon from "../../assets/common/delete.svg";
+import shareIcon from "../../assets/common/share.svg";
+import bookmarkIcon from "../../assets/common/bookmark.svg";
 import Subject from "../MainPage/Subject";
-const PatientCase = ({ patientInfo }) => {
+//isCustom: 내가 만든 케이스, isChecked: 체크 on/off, isCompleted: 문진 완료
+const PatientCase = ({
+  patientInfo,
+  isCustom = false,
+  isChecked = false,
+  isCompleted = false,
+}) => {
   return (
-    <div className="flex items-center gap-2 py-4 border-b border-b-cgray-300 cursor-pointer">
-      <Subject subject="문진 전" />
-      {patientInfo}
+    <div className="flex justify-between items-center py-4 border-b border-b-cgray-300 cursor-pointer">
+      <div className="flex items-center gap-2">
+        <Subject subject="문진 전" />
+        <span className="w-[15.625rem]">{patientInfo}</span>
+        {isCustom && (
+          <div className="flex items-center gap-1">
+            <button
+              className="w-7 h-7"
+              style={{ backgroundImage: `url(${deleteIcon})` }}
+            />
+            <button
+              className="w-7 h-7"
+              style={{ backgroundImage: `url(${shareIcon})` }}
+            />
+          </div>
+        )}
+      </div>
+      <div className="flex items-center gap-2">
+        {isChecked && (
+          <button
+            className="w-7 h-7"
+            style={{ backgroundImage: `url(${bookmarkIcon})` }}
+          />
+        )}
+        {isCompleted && (
+          <button className="px-3 text-p2 text-cgray-700">{`피드백 >`}</button>
+        )}
+      </div>
     </div>
   );
 };
